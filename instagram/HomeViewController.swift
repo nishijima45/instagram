e428f11527d1c21a98a5d2a68399f1455ba56165
西嶋信吾 <tree-frog@live.jp>
2018-07-16 20:53:36 +0900

    gitignoreまで

--- instagram/HomeViewController.swift
+++ instagram/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  instagram
 //
 //  Created by 西嶋 信吾 on 2018/07/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
